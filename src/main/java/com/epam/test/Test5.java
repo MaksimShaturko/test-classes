2779a9fc22d7a69be1c80b676a86838d83336fdd
Maksim Shaturko <shaturko.maksim@gmail.com>
2022-02-23 15:30:53 +0300

    Add Test5. testing static and non-static blocks and constructors when use extending. Show sequence of console output

---
+++ src/main/java/com/epam/test/Test5.java
@@ -0,0 +1,47 @@
+package com.epam.test;
+
+public class Test5 {
+    static {
+        System.out.println("static block in Test5");
+    }
+
+    {
+        System.out.println("logic block in Test5");
+    }
+
+    Test5(){
+        System.out.println("Constructor Test5");
+    }
+
+    public static void main(String[] args) {
+        B5 b = new B5();
+    }
+}
+
+class A5 extends Test5{
+    static {
+        System.out.println("static block in A5");
+    }
+
+    {
+        System.out.println("logic block in A5");
+    }
+
+    A5(){
+        System.out.println("Constructor A5");
+    }
+}
+
+class B5 extends A5{
+    static {
+        System.out.println("static block in B5");
+    }
+
+    {
+        System.out.println("logic block in B5");
+    }
+
+    B5(){
+        System.out.println("Constructor B5");
+    }
+}
